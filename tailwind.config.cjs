72ae6c077d813fa79d6c318de9066c5daf2c1a1e
Aiden Wood <aidenthomasgodbywood@gmail.com>
2023-10-08 11:51:53 +1000

    XX

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
       current: 'currentColor',
       transparent: 'transparent',
       white: '#ffffff',
-      primary: colors.stone,
+      primary: colors.violet,
     },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
