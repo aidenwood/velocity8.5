d69a9abfe122ebfc78fd7e98d211d017847f6987
Aiden Wood <aidenthomasgodbywood@gmail.com>
2023-10-13 10:10:19 +1000

    Chuururr

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -28,8 +28,8 @@ module.exports = {
       'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1480px',
-      // => @media (min-width: 1280px) { ... }
+      'xl': '1680px',
+      // => @media (min-width: 1680px) { ... }
 
       '2xl': '1986px',
       // => @media (min-width: 1536px) { ... }
